db876b4ed4d6d35b7e67c5ac328ff7dcf1fd4697
jguice <jguice@gmail.com>
2014-06-16 16:34:12 -0700

    add laravel steps for mygoutstudy

--- stacks/mygoutstudy.rb
+++ stacks/mygoutstudy.rb
@@ -48,10 +48,10 @@ module Deployinator
           run_cmd %Q{rsync -av --exclude='.git/' --exclude='.gitignore' #{mygoutstudy_git_checkout_path}/ #{site_path}}
           # set permissions so webserver can write TODO setup passwordless sudo to chown&chmod instead? or
             # maybe set CAP_CHOWN for deployinator?
-          # run_cmd %Q{chmod 777 #{site_path}/files}
-          # run_cmd %Q{chmod 777 #{site_path}/app/storage/*}
-          # run_cmd %Q{cd #{site_path} && /usr/local/bin/composer install}
-          # run_cmd %Q{cd #{site_path} && /usr/local/bin/composer dump-autoload}
+          run_cmd %Q{chmod 777 #{site_path}/files}
+          run_cmd %Q{chmod 777 #{site_path}/app/storage/*}
+          run_cmd %Q{cd #{site_path} && /usr/local/bin/composer install}
+          run_cmd %Q{cd #{site_path} && /usr/local/bin/composer dump-autoload}
           log_and_stream "Done!<br>"
         rescue
           log_and_stream "Failed!<br>"
