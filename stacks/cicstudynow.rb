7dab8aa725f3f58a5423342abe1680d31887f727
jguice <jguice@gmail.com>
2014-11-13 17:51:11 -0800

    updated cicstudy to match latest standard laravel steps

--- stacks/cicstudynow.rb
+++ stacks/cicstudynow.rb
@@ -63,17 +63,20 @@ module Deployinator
           # take application offline (maintenance mode)
           run_cmd %Q{cd #{site_path} && /usr/bin/php artisan down || true} # return true so command is non-fatal
 
-          # sync files to final destination
-          run_cmd %Q{rsync -av --delete --force --include='app/storage/meta/services.json'  --exclude='app/storage/*' --exclude='vendor/*' --exclude='.git/' --exclude='.gitignore' #{cicstudynow_git_checkout_path}/ #{site_path}}
+          # sync site files to final destination
+          run_cmd %Q{rsync -av --delete --force --exclude='app/storage/' --exclude='vendor/' --exclude='.git/' --exclude='.gitignore' #{cicstudynow_git_checkout_path}/ #{site_path}}
+
+          # additionally sync top-level storage dirs (but not their contents)
+          run_cmd %Q{rsync -lptgoDv --dirs --delete --force --exclude='.gitignore' #{cicstudynow_git_checkout_path}/app/storage/ #{site_path}/app/storage}
 
           # ensure storage is writable (shouldn't have to do this but running webserver as different user)
-          run_cmd %Q{chmod -R 777 #{site_path}/app/storage}
+          run_cmd %Q{chmod 777 #{site_path}/app/storage/*}
 
           # install dependencies (vendor dir was probably completely removed via above)
           run_cmd %Q{cd #{site_path} && /usr/local/bin/composer install --no-dev}
 
           # run db migrations
-          #run_cmd %Q{cd #{site_path} && /usr/bin/php artisan migrate}
+          #run_cmd %Q{cd #{site_path} && /usr/bin/php artisan migrate} # NOTE no db migrations for this site
 
           # put application back online
           run_cmd %Q{cd #{site_path} && /usr/bin/php artisan up}
@@ -93,13 +96,15 @@ module Deployinator
 
         begin
           # take application offline (maintenance mode)
-          run_cmd %Q{ssh #{cicstudynow_prod_user}@#{cicstudynow_prod_ip} "cd #{site_path} && /usr/bin/php artisan down"}
+          run_cmd %Q{ssh #{cicstudynow_prod_user}@#{cicstudynow_prod_ip} "cd #{site_path} && /usr/bin/php artisan down || true"} # return true so command is non-fatal (artisan doesn't exist the first time)
+
+          # TODO figure out how to keep from deleting cicstudynow/app/storage/meta/down (which enables the site)
 
           # sync new app contents
           run_cmd %Q{rsync -ave ssh --delete --force --delete-excluded #{site_path} #{cicstudynow_prod_user}@#{cicstudynow_prod_ip}:#{site_root}}
 
           # run database migrations
-          run_cmd %Q{ssh #{cicstudynow_prod_user}@#{cicstudynow_prod_ip} "cd #{site_path} && /usr/bin/php artisan migrate --force"}
+          #run_cmd %Q{ssh #{cicstudynow_prod_user}@#{cicstudynow_prod_ip} "cd #{site_path} && /usr/bin/php artisan migrate --force"} # NOTE no db migrations for this site
 
           # generate optimized autoload files
           run_cmd %Q{ssh #{cicstudynow_prod_user}@#{cicstudynow_prod_ip} "cd #{site_path} && /usr/local/bin/composer dump-autoload -o"}
