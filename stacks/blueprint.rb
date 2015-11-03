4f513fb98883a65f08c7ce8b09b8678d5873e275
jguice <jguice@gmail.com>
2015-11-03 07:00:57 -0800

    blueprint2 is now the only blueprint

--- stacks/blueprint.rb
+++ stacks/blueprint.rb
@@ -1,12 +1,16 @@
 module Deployinator
   module Stacks
-    module Blueprint
+    module blueprint
       def blueprint_git_repo_url
         "git@github.com:xfactoradvertising/blueprint.git"
       end
 
-      def blueprint_prod_user
+      def blueprint_user
         'www-data'
+      end
+
+      def blueprint_stage_ip
+        '52.25.81.13'
       end
 
       def blueprint_prod_ip
@@ -29,16 +33,16 @@ module Deployinator
         "#{checkout_root}/#{stack}"
       end
 
-      def blueprint_dev_version
-        %x{cat #{blueprint_git_checkout_path}/version.txt}
+      def blueprint_stage_version
+        %x{ssh #{blueprint_user}@#{blueprint_stage_ip} 'cat #{site_path}/version.txt'}
       end
 
-      def blueprint_dev_build
-        Version.get_build(blueprint_dev_version)
+      def blueprint_stage_build
+        Version.get_build(blueprint_stage_version)
       end
 
       def blueprint_prod_version
-        %x{ssh #{blueprint_prod_user}@#{blueprint_prod_ip} 'cat #{site_path}/version.txt'}
+        %x{ssh #{blueprint_user}@#{blueprint_prod_ip} 'cat #{site_path}/version.txt'}
       end
 
       def blueprint_prod_build
@@ -46,14 +50,18 @@ module Deployinator
       end
 
       def blueprint_head_build
-        %x{git ls-remote #{blueprint_git_repo_url} HEAD | cut -c1-7}.chomp
+        # NOTE explicitly getting branch HEAD revision here (could move to a helper)
+        # %x{git ls-remote #{blueprint_git_repo_url} HEAD | cut -c1-7}.chomp
+        %x{git ls-remote #{blueprint_git_repo_url} blueprint HEAD | tail -1 | cut -c1-7}.chomp
       end
 
-      def blueprint_dev(options={})
-        old_build = Version.get_build(blueprint_dev_version)
+      def blueprint_stage(options={})
+        old_build = blueprint_stage_build
 
-        git_cmd = old_build ? :git_freshen_clone : :github_clone
-        send(git_cmd, stack, 'sh -c')
+        #git_cmd = old_build ? :git_freshen_clone : :github_clone
+        git_cmd = old_build ? :git_freshen_clone_branch : :github_clone_branch
+        #send(git_cmd, stack, 'sh -c')
+        send(git_cmd, stack, 'blueprint', 'sh -c')
 
         git_bump_version stack, ''
 
@@ -61,54 +69,50 @@ module Deployinator
 
         begin
           # take application offline (maintenance mode)
-          run_cmd %Q{cd #{site_path} && /usr/bin/php artisan down || true} # return true so command is non-fatal
+          # return true so command is non-fatal (artisan doesn't exist the first time)
+          run_cmd %Q{ssh #{blueprint_user}@#{blueprint_stage_ip} "cd #{site_path} && /usr/bin/php artisan down --env=stage || true"}
 
-          # sync site files to final destination
-          run_cmd %Q{rsync -av --delete --force --exclude='app/storage/' --exclude='/vendor/' --exclude='.git/' --exclude='.gitignore' #{blueprint_git_checkout_path}/ #{site_path}}
+          # sync new app contents
+          run_cmd %Q{rsync -ave ssh --delete --force --exclude='storage/*/*/**' --exclude='vendor/' --exclude='.git/' --exclude='.gitignore' --exclude='.env' --filter "protect .env" --filter "protect down" --filter "protect vendor/" --filter "protect storage/*/**" #{blueprint_git_checkout_path}/ #{blueprint_user}@#{blueprint_stage_ip}:#{site_path}}
 
-          # additionally sync top-level storage dirs (but not their contents)
-          run_cmd %Q{rsync -lptgoDv --dirs --delete --force --exclude='.gitignore' #{blueprint_git_checkout_path}/app/storage/ #{site_path}/app/storage}
-
-          # ensure storage is writable (shouldn't have to do this but running webserver as different user)
-          run_cmd %Q{chmod 777 #{site_path}/app/storage/*}
-
-          # install dependencies (vendor dir was probably completely removed via above)
-          run_cmd %Q{cd #{site_path} && /usr/local/bin/composer install --no-dev}
+          # install dependencies
+          run_cmd %Q{ssh #{blueprint_user}@#{blueprint_stage_ip} "cd #{site_path} && /usr/local/bin/composer install --no-dev"}
 
           # run db migrations
-          run_cmd %Q{cd #{site_path} && /usr/bin/php artisan migrate}
+          run_cmd %Q{ssh #{blueprint_user}@#{blueprint_stage_ip} "cd #{site_path} && /usr/bin/php artisan migrate:refresh --seed --env=stage"}
 
           # put application back online
-          run_cmd %Q{cd #{site_path} && /usr/bin/php artisan up}
+          run_cmd %Q{ssh #{blueprint_user}@#{blueprint_stage_ip} "cd #{site_path} && /usr/bin/php artisan up --env=stage"}
 
           log_and_stream "Done!<br>"
         rescue
           log_and_stream "Failed!<br>"
         end
 
-        log_and_shout(:old_build => old_build, :build => build, :send_email => false) # TODO make email true
+        log_and_shout(:old_build => old_build, :build => build, :env => 'STAGE', :send_email => false) # TODO make email true
 
       end
 
       def blueprint_prod(options={})
-        old_build = Version.get_build(blueprint_prod_version)
-        build = blueprint_dev_build
+        old_build = blueprint_prod_build
+        build = blueprint_stage_build
 
         begin
           # take application offline (maintenance mode)
-          run_cmd %Q{ssh #{blueprint_prod_user}@#{blueprint_prod_ip} "cd #{site_path} && /usr/bin/php artisan down || true"} # return true so command is non-fatal (artisan doesn't exist the first time)
+          # return true so command is non-fatal (artisan doesn't exist the first time)
+          run_cmd %Q{ssh #{blueprint_user}@#{blueprint_prod_ip} "cd #{site_path} && /usr/bin/php artisan down || true"}
 
           # sync new app contents
-          run_cmd %Q{rsync -ave ssh --delete --force --delete-excluded #{site_path} #{blueprint_prod_user}@#{blueprint_prod_ip}:#{site_root}}
+          run_cmd %Q{ssh #{blueprint_user}@#{blueprint_stage_ip} "rsync -ave ssh --delete --force --exclude='storage/*/*/**' --exclude='storage/*/**' --exclude='.env' --filter 'protect .env' --filter 'protect down' --filter 'protect storage/*/**' #{site_path}/ #{blueprint_user}@#{blueprint_prod_ip}:#{site_path}"}
 
           # run database migrations
-          run_cmd %Q{ssh #{blueprint_prod_user}@#{blueprint_prod_ip} "cd #{site_path} && /usr/bin/php artisan migrate --force"}
+          run_cmd %Q{ssh #{blueprint_user}@#{blueprint_prod_ip} "cd #{site_path} && /usr/bin/php artisan migrate --seed"}
 
           # generate optimized autoload files
-          run_cmd %Q{ssh #{blueprint_prod_user}@#{blueprint_prod_ip} "cd #{site_path} && /usr/local/bin/composer dump-autoload -o"}
+          run_cmd %Q{ssh #{blueprint_user}@#{blueprint_prod_ip} "cd #{site_path} && /usr/local/bin/composer dump-autoload -o"}
 
           # take application online
-          run_cmd %Q{ssh #{blueprint_prod_user}@#{blueprint_prod_ip} "cd #{site_path} && /usr/bin/php artisan up"}
+          run_cmd %Q{ssh #{blueprint_user}@#{blueprint_prod_ip} "cd #{site_path} && /usr/bin/php artisan up"}
 
           log_and_stream "Done!<br>"
         rescue
@@ -118,23 +122,22 @@ module Deployinator
         log_and_shout(:old_build => old_build, :build => build, :env => 'PROD', :send_email => false) # TODO make email true
       end
 
-      # blueprint prod is left purely as an example
       def blueprint_environments
         [
           {
-            :name => 'dev',
-            :method => 'blueprint_dev',
-            :current_version => blueprint_dev_version,
-            :current_build => blueprint_dev_build,
+            :name => 'stage',
+            :method => 'blueprint_stage',
+            :current_version => blueprint_stage_version,
+            :current_build => blueprint_stage_build,
             :next_build => blueprint_head_build
-          }#,
-          # {
-          #   :name => 'prod',
-          #   :method => 'blueprint_prod',
-          #   :current_version => blueprint_prod_version,
-          #   :current_build => blueprint_prod_build,
-          #   :next_build => blueprint_dev_build
-          # }        
+          },
+          {
+            :name => 'prod',
+            :method => 'blueprint_prod',
+            :current_version => blueprint_prod_version,
+            :current_build => blueprint_prod_build,
+            :next_build => blueprint_stage_build
+          }        
         ]
       end
     end
