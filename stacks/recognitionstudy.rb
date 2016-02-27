0ae37323e6d84d86004c0922cdce93c5623a5d63
Josh Guice <jguice@gmail.com>
2016-02-27 10:31:49 -0800

    refactor(recognitionstudy): update for new deployinator

--- stacks/recognitionstudy.rb
+++ stacks/recognitionstudy.rb
@@ -2,11 +2,15 @@ module Deployinator
   module Stacks
     module Recognitionstudy
       def recognitionstudy_git_repo_url
-        "git@github.com:xfactoradvertising/recognitionstudy.git"
+        'git@github.com:xfactoradvertising/recognitionstudy.git'
       end
 
-      def recognitionstudy_prod_user
+      def recognitionstudy_user
         'www-data'
+      end
+
+      def recognitionstudy_stage_ip
+        '52.25.81.13'
       end
 
       def recognitionstudy_prod_ip
@@ -14,7 +18,7 @@ module Deployinator
       end
 
       def checkout_root
-        "/tmp"
+        '/tmp'
       end
 
       def site_root
@@ -30,7 +34,7 @@ module Deployinator
       end
 
       def recognitionstudy_stage_version
-        %x{cat #{recognitionstudy_git_checkout_path}/version.txt}
+        %x{ssh #{recognitionstudy_user}@#{recognitionstudy_stage_ip} 'cat #{site_path}/version.txt'}
       end
 
       def recognitionstudy_stage_build
@@ -38,7 +42,7 @@ module Deployinator
       end
 
       def recognitionstudy_prod_version
-        %x{ssh #{recognitionstudy_prod_user}@#{recognitionstudy_prod_ip} 'cat #{site_path}/version.txt'}
+        %x{ssh #{recognitionstudy_user}@#{recognitionstudy_prod_ip} 'cat #{site_path}/version.txt'}
       end
 
       def recognitionstudy_prod_build
@@ -49,8 +53,9 @@ module Deployinator
         %x{git ls-remote #{recognitionstudy_git_repo_url} HEAD | cut -c1-7}.chomp
       end
 
+      # NOTE this options hash is unused but it is still passed by deployinator and removing it will break things
       def recognitionstudy_stage(options={})
-        old_build = Version.get_build(recognitionstudy_stage_version)
+        old_build = recognitionstudy_stage_build
 
         git_cmd = old_build ? :git_freshen_clone : :github_clone
         send(git_cmd, stack, 'sh -c')
@@ -61,59 +66,58 @@ module Deployinator
 
         begin
           # take application offline (maintenance mode)
-          run_cmd %Q{cd #{site_path} && /usr/bin/php artisan down --env=stage || true} # return true so command is non-fatal
+          # return true so command is non-fatal (artisan doesn't exist the first time)
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_stage_ip} "cd #{site_path} && /usr/bin/php artisan down --env=stage || true"}
 
-          # sync site files to final destination
-          run_cmd %Q{rsync -av --delete --force --exclude='app/storage/' --exclude='/vendor/' --exclude='.git/' --exclude='.gitignore' --filter "protect .env*" #{recognitionstudy_git_checkout_path}/ #{site_path}}
+          # sync new app contents
+          run_cmd %Q{rsync -ave ssh --delete --force --exclude='app/storage/*/**' --exclude='vendor/' --exclude='.git/' --exclude='.gitignore' --exclude='.env*' --filter 'protect .env*' --filter 'protect down' --filter "protect vendor/" --filter 'protect app/storage/**' #{recognitionstudy_git_checkout_path}/ #{recognitionstudy_user}@#{recognitionstudy_stage_ip}:#{site_path}}
 
-          # additionally sync top-level storage dirs (but not their contents)
-          run_cmd %Q{rsync -lptgoDv --dirs --delete --force --exclude='.gitignore' #{recognitionstudy_git_checkout_path}/app/storage/ #{site_path}/app/storage}
+          # install dependencies
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_stage_ip} "cd #{site_path} && /usr/local/bin/composer install --no-stage"}
 
-          # ensure storage is writable (shouldn't have to do this but running webserver as different user)
-          run_cmd %Q{chmod 777 #{site_path}/app/storage/*}
-
-          # install dependencies (vendor dir was probably completely removed via above)
-          run_cmd %Q{cd #{site_path} && /usr/local/bin/composer install --no-dev}
+          # generate optimized autoload files
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_stage_ip} "cd #{site_path} && /usr/local/bin/composer dump-autoload -o"}
 
           # run db migrations
-          run_cmd %Q{cd #{site_path} && /usr/bin/php artisan migrate --seed --env=stage}
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_stage_ip} "cd #{site_path} && /usr/bin/php artisan migrate --seed --env=stage"}
 
           # put application back online
-          run_cmd %Q{cd #{site_path} && /usr/bin/php artisan up --env=stage}
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_stage_ip} "cd #{site_path} && /usr/bin/php artisan up --env=stage"}
 
-          log_and_stream "Done!<br>"
+          log_and_stream 'Done!<br>'
         rescue
-          log_and_stream "Failed!<br>"
+          log_and_stream 'Failed!<br>'
         end
 
-        log_and_shout(:old_build => old_build, :build => build, :send_email => false) # TODO make email true
+        log_and_shout(:old_build => old_build, :build => build, :env => 'STAGE', :send_email => false) # TODO make email true
 
       end
 
+      # NOTE this options hash is unused but it is still passed by deployinator and removing it will break things
       def recognitionstudy_prod(options={})
-        old_build = Version.get_build(recognitionstudy_prod_version)
+        old_build = recognitionstudy_prod_build
         build = recognitionstudy_stage_build
 
         begin
           # take application offline (maintenance mode)
           # return true so command is non-fatal (artisan doesn't exist the first time)
-          run_cmd %Q{ssh #{recognitionstudy_prod_user}@#{recognitionstudy_prod_ip} "cd #{site_path} && /usr/bin/php artisan down || true"}
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_prod_ip} "cd #{site_path} && /usr/bin/php artisan down || true"}
 
           # sync new app contents
-          run_cmd %Q{rsync -ave ssh --delete --force --delete-excluded #{site_path} --filter "protect .env.php" --filter "protect down" #{recognitionstudy_prod_user}@#{recognitionstudy_prod_ip}:#{site_root}}
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_stage_ip} "cd #{site_path} && rsync -ave ssh --delete --force --delete-excluded --exclude='app/storage/*/**' --exclude='.env*' --filter 'protect .env*' --filter 'protect down' --filter 'protect app/storage/**' #{site_path}/ #{recognitionstudy_user}@#{recognitionstudy_prod_ip}:#{site_path}"}
 
           # generate optimized autoload files
-          run_cmd %Q{ssh #{recognitionstudy_prod_user}@#{recognitionstudy_prod_ip} "cd #{site_path} && /usr/local/bin/composer dump-autoload -o"}
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_prod_ip} "cd #{site_path} && /usr/local/bin/composer dump-autoload -o"}
 
-          # run db migrations
-          run_cmd %Q{ssh #{recognitionstudy_prod_user}@#{recognitionstudy_prod_ip} "cd #{site_path} && /usr/bin/php artisan migrate --force --seed"}
+          # run database migrations
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_prod_ip} "cd #{site_path} && /usr/bin/php artisan migrate --force --seed"}
 
           # take application online
-          run_cmd %Q{ssh #{recognitionstudy_prod_user}@#{recognitionstudy_prod_ip} "cd #{site_path} && /usr/bin/php artisan up"}
+          run_cmd %Q{ssh #{recognitionstudy_user}@#{recognitionstudy_prod_ip} "cd #{site_path} && /usr/bin/php artisan up"}
 
-          log_and_stream "Done!<br>"
+          log_and_stream 'Done!<br>'
         rescue
-          log_and_stream "Failed!<br>"
+          log_and_stream 'Failed!<br>'
         end
 
         log_and_shout(:old_build => old_build, :build => build, :env => 'PROD', :send_email => false) # TODO make email true
@@ -121,20 +125,20 @@ module Deployinator
 
       def recognitionstudy_environments
         [
-          {
-            :name => 'stage',
-            :method => 'recognitionstudy_stage',
-            :current_version => recognitionstudy_stage_version,
-            :current_build => recognitionstudy_stage_build,
-            :next_build => recognitionstudy_head_build
-          },
-          {
-            :name => 'prod',
-            :method => 'recognitionstudy_prod',
-            :current_version => recognitionstudy_prod_version,
-            :current_build => recognitionstudy_prod_build,
-            :next_build => recognitionstudy_stage_build
-          }        
+            {
+                :name => 'stage',
+                :method => 'recognitionstudy_stage',
+                :current_version => recognitionstudy_stage_version,
+                :current_build => recognitionstudy_stage_build,
+                :next_build => recognitionstudy_head_build
+            },
+            {
+                :name => 'prod',
+                :method => 'recognitionstudy_prod',
+                :current_version => recognitionstudy_prod_version,
+                :current_build => recognitionstudy_prod_build,
+                :next_build => recognitionstudy_stage_build
+            }
         ]
       end
     end
