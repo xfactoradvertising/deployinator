e9e987ef7b92b6c3eedc016ec041206faf09dc0a
Josh Guice <jguice@gmail.com>
2016-10-25 21:20:56 -0700

    switching smart5 to stage

--- stacks/smart5.rb
+++ stacks/smart5.rb
@@ -9,8 +9,8 @@ module Deployinator
         'www-data'
       end
 
-      def smart5_stage_ip
-        '52.25.81.13'
+      def smart5_dev_ip
+        '52.32.55.10'
       end
 
       def smart5_prod_ip
@@ -33,12 +33,12 @@ module Deployinator
         "#{checkout_root}/#{stack}"
       end
 
-      def smart5_stage_version
-        %x{ssh #{smart5_user}@#{smart5_stage_ip} 'cat #{site_path}/version.txt'}
+      def smart5_dev_version
+        %x{ssh #{smart5_user}@#{smart5_dev_ip} 'cat #{site_path}/version.txt'}
       end
 
-      def smart5_stage_build
-        Version.get_build(smart5_stage_version)
+      def smart5_dev_build
+        Version.get_build(smart5_dev_version)
       end
 
       def smart5_prod_version
@@ -56,8 +56,8 @@ module Deployinator
         %x{git ls-remote #{smart5_git_repo_url} smart5 HEAD | tail -1 | cut -c1-7}.chomp
       end
 
-      def smart5_stage(options={})
-        old_build = smart5_stage_build
+      def smart5_dev(options={})
+        old_build = smart5_dev_build
 
         git_cmd = old_build ? :git_freshen_clone_branch : :github_clone_branch
         send(git_cmd, stack, 'smart5', 'sh -c')
@@ -69,32 +69,32 @@ module Deployinator
         begin
           # take application offline (maintenance mode)
           # return true so command is non-fatal (artisan doesn't exist the first time)
-          run_cmd %Q{ssh #{smart5_user}@#{smart5_stage_ip} "cd #{site_path} && /usr/bin/php artisan down --env=stage || true"}
+          run_cmd %Q{ssh #{smart5_user}@#{smart5_dev_ip} "cd #{site_path} && /usr/bin/php artisan down --env=dev || true"}
 
           # sync new app contents
-          run_cmd %Q{rsync -ave ssh --delete --force --exclude='storage/*/*/**' --exclude='vendor/' --exclude='.git/' --exclude='.gitignore' --exclude='.env' --filter "protect .env" --filter "protect down" --filter "protect vendor/" --filter "protect storage/*/**" #{smart5_git_checkout_path}/ #{smart5_user}@#{smart5_stage_ip}:#{site_path}}
+          run_cmd %Q{rsync -ave ssh --delete --force --exclude='storage/*/*/**' --exclude='vendor/' --exclude='.git/' --exclude='.gitignore' --exclude='.env' --filter "protect .env" --filter "protect down" --filter "protect vendor/" --filter "protect storage/*/**" #{smart5_git_checkout_path}/ #{smart5_user}@#{smart5_dev_ip}:#{site_path}}
 
           # install dependencies
-          run_cmd %Q{ssh #{smart5_user}@#{smart5_stage_ip} "cd #{site_path} && /usr/local/bin/composer install --no-dev"}
+          run_cmd %Q{ssh #{smart5_user}@#{smart5_dev_ip} "cd #{site_path} && /usr/local/bin/composer install --no-dev"}
 
           # run db migrations
-          run_cmd %Q{ssh #{smart5_user}@#{smart5_stage_ip} "cd #{site_path} && /usr/bin/php artisan migrate --seed --env=stage"}
+          run_cmd %Q{ssh #{smart5_user}@#{smart5_dev_ip} "cd #{site_path} && /usr/bin/php artisan migrate --seed --env=dev"}
 
           # put application back online
-          run_cmd %Q{ssh #{smart5_user}@#{smart5_stage_ip} "cd #{site_path} && /usr/bin/php artisan up --env=stage"}
+          run_cmd %Q{ssh #{smart5_user}@#{smart5_dev_ip} "cd #{site_path} && /usr/bin/php artisan up --env=dev"}
 
           log_and_stream "Done!<br>"
         rescue
           log_and_stream "Failed!<br>"
         end
 
-        log_and_shout(:old_build => old_build, :build => build, :env => 'STAGE', :send_email => false) # TODO make email true
+        log_and_shout(:old_build => old_build, :build => build, :env => 'DEV', :send_email => false) # TODO make email true
 
       end
 
       def smart5_prod(options={})
         old_build = smart5_prod_build
-        build = smart5_stage_build
+        build = smart5_dev_build
 
         begin
           # take application offline (maintenance mode)
@@ -102,7 +102,7 @@ module Deployinator
           run_cmd %Q{ssh #{smart5_user}@#{smart5_prod_ip} "cd #{site_path} && /usr/bin/php artisan down || true"}
 
           # sync new app contents
-          run_cmd %Q{ssh #{smart5_user}@#{smart5_stage_ip} "rsync -ave ssh --delete --force --exclude='storage/*/*/**' --exclude='storage/*/**' --exclude='.env' --filter 'protect .env' --filter 'protect down' --filter 'protect storage/*/**' #{site_path}/ #{smart5_user}@#{smart5_prod_ip}:#{site_path}"}
+          run_cmd %Q{ssh #{smart5_user}@#{smart5_dev_ip} "rsync -ave ssh --delete --force --exclude='storage/*/*/**' --exclude='storage/*/**' --exclude='.env' --filter 'protect .env' --filter 'protect down' --filter 'protect storage/*/**' #{site_path}/ #{smart5_user}@#{smart5_prod_ip}:#{site_path}"}
 
           # generate optimized autoload files
           run_cmd %Q{ssh #{smart5_user}@#{smart5_prod_ip} "cd #{site_path} && /usr/local/bin/composer dump-autoload -o"}
@@ -124,10 +124,10 @@ module Deployinator
       def smart5_environments
         [
           {
-            :name => 'stage',
-            :method => 'smart5_stage',
-            :current_version => smart5_stage_version,
-            :current_build => smart5_stage_build,
+            :name => 'dev',
+            :method => 'smart5_dev',
+            :current_version => smart5_dev_version,
+            :current_build => smart5_dev_build,
             :next_build => smart5_head_build
           }#,
           # {
